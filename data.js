a7b080713653d6c158bfc88a141ebf81039b3794
E-RKP <edobackup111@gmail.com>
2025-05-30 13:35:40 +0200

    oop 3

--- data.js
+++ data.js
@@ -1,17 +1,44 @@
 function Persona (nome, cognome, età, genere, interessi){
 
-  Persona.nome = nome,
-  Persona.cognome = cognome,
+  this.nome = nome,
+  this.cognome = cognome,
   this.età = età,
   this.genere = genere,
   this.interessi = interessi
   this.bio = function(){
-    console.log(`${this.nome}  ${this.cognome} è ${(this.genere === 'Maschio') ? 'un ragazzo' : 'una ragazza'} di ${this.età} anni, i suoi interessi sono ${this.interessi.join(', ')}.`)
+    return `${this.nome}  ${this.cognome} è ${(this.genere === 'Maschio') ? 'un ragazzo' : 'una ragazza'} di ${this.età} anni, i suoi interessi sono ${(this.interessi.length === 2) ? this.interessi.join(' e ') : this.interessi.join(', ')}.`
   }
   this.saluta = function (){
-    console.log(`Yo! Sono ${this.nome}`)
+    return `Yo! Sono ${this.nome}`
   }
+}
 
+
+let persona1 = new Persona('Luca', 'Gialli', 19, 'Maschio', ['calcio', 'ping pong', 'tennis'])
+let persona2 = new Persona ('Maria', 'Dandi', 22, 'Femmina', ['danza', 'Instagram'])
+
+console.log(persona1)
+console.log(persona1.bio())
+console.log(persona1.saluta())
+console.log(persona2)
+console.log(persona2.bio())
+console.log(persona2.saluta())
+
+
+function Animale (specie, nome, età){
+
+  this.specie = specie
+  this.nome = nome
+  this.età = età
+  this.bio = function (){
+    return `Il ${this.specie} di nome ${this.nome} ha ${this.età} anni`
+  }
 
 }
 
+let animale1 = new Animale('canide', 'pazzo', 50)
+let animale2 = new Animale ('gattastro', 'Sole', 2)
+
+console.log(animale1)
+console.log(animale1.bio())
+console.log(animale2.bio())
